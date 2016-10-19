949cc562d28dda0f7d2fd9f19284d33ac81d4a8c
Leah Bueing <lbueing@gmail.com>
2016-10-19 16:06:54 -0700

    added params to controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,6 +4,16 @@ class TransactionsController < ApplicationController
   end
 
   def create
+    @mytransaction = Transaction.new
+    @mytransaction.client_name = params[:client_name]
+    @mytransaction.client_email = params[:client_email]
+    @mytransaction.client_address = params[:client_address]
+    @mytransaction.client_cc_num = params[:client_cc_num]
+    @mytransaction.client_cc_exp = params[:client_cc_exp]
+    @mytransaction.status = nil
+    @mytransaction.total_price = nil
+    # @mytransaction.save
+
   end
 
   def show
@@ -11,4 +21,10 @@ class TransactionsController < ApplicationController
 
   def show_all
   end
+
+  private
+  def user_params
+    params.require(:transaction).permit(:client_name, :client_email, :status, :)
+  end
+
 end
