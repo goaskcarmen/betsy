49c4a7386fa7aa9f27b339b188233944723f337e
Carmen Chan <carmenkellychan@gmail.com>
2016-10-24 16:45:25 -0700

    added more seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,17 @@
 require 'csv'
 
 CSV.foreach('seed_csvs/products.csv', :headers => true ) do |prod_obj|
-  Product.create(name: prod_obj["name"], unit_price: prod_obj["unit_price"], quantity: prod_obj["quantity"], description: prod_obj["description"], photo_url: prod_obj["photo_url"])
+  Product.create(id: prod_obj["product_id"], name: prod_obj["name"], unit_price: prod_obj["unit_price"], quantity: prod_obj["quantity"], description: prod_obj["description"], photo_url: prod_obj["photo_url"])
+end
+
+CSV.foreach('seed_csvs/users.csv', :headers => true ) do |user_obj|
+  User.create(id: user_obj["user_id"], name: user_obj["name"], email: user_obj["email"], provider: user_obj["provider"], uid: user_obj["uid"])
+end
+
+CSV.foreach('seed_csvs/categories.csv', :headers => true ) do |cat_obj|
+  Category.create(id: cat_obj["category_id"], name: cat_obj["name"])
+end
+
+CSV.foreach('seed_csvs/categories_products.csv', :headers => true ) do |cat_prod|
+  Product.find(cat_prod["product_id"]).categories << Category.find(cat_prod["category_id"])
 end
