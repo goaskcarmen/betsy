5d8848007aefec6c898935903060689e454d376e
Qlwentt <qlwentt@gmail.com>
2016-10-24 13:31:16 -0700

    fixes login/register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,25 +3,36 @@ class UsersController < ApplicationController
 
 
   def logged_in_index
-   @user = User.find(params[:id])
+    if @user==nil
+      flash[:notice]="please log in to view your account page"
+      return redirect_to :back
+    end
+
    @reviews = @user.reviews
   end
 
   def new
-    if @user
+    #if they hit "regitster first", redirect to the github auth action in sessions
+    auth_hash=session[:auth_hash]
+    if session[:auth_hash] == nil 
+      return redirect_to "/auth/github" 
+    end
+
+    #if they are already a current user, tell them so
+    if User.find_by(uid: auth_hash['uid'], provider: 'github')
       flash[:notice]="You are already registered with github"
       return redirect_to :back
     end
 
-    if session[:auth_hash] == nil 
-      return redirect_to "/auth/github" 
-    end
+    #if they have a github auth build potential user info from github auth
     @user=User.build_from_github(session[:auth_hash])
   end
 
   def create
     @user=User.new
     flash[:notice] = "unable to save user"
+
+    #take user info from form
     @user.name = params[:user][:name]
     @user.email = params[:user][:email]
     @user.uid = params[:user][:uid]
@@ -55,6 +66,7 @@ class UsersController < ApplicationController
   private
 
   def set_current_user
+    #This is the logged in user
     @user= User.find_by(id: session[:user_id])
   end
 end
